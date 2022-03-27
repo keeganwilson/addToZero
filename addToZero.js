8cb03b1f44f6a3d08f0b076a832141ab28a19de3
Keegan Wilson <keegan.wilson@gmail.com>
2022-03-27 08:02:11 -0600

    finished challenge

--- addToZero.js
+++ addToZero.js
@@ -1,4 +1,18 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 12]
+let array = [28, 43, -12, 30, 4, 0, 1]
 
 // Write your solution below:
+
+const addToZero = (arr) => {
+    let hasOpp = 'False'
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++)
+            if (i !== j) {
+                if(arr[i] === -arr[j])
+            hasOpp = 'True'
+        }
+    }
+    console.log(hasOpp)
+}
+
+addToZero(array);
